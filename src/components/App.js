4877c8be55eb9dea9c2eb0ba41ce53c2857efbd7
Muriithi Ian <imuriithiian@gmail.com>
2022-09-26 23:37:46 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,14 @@ import MovieCard from "./MovieCard";
 function App() {
   const title = "Mad Max";
   const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
-  const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+	const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
-  return (
-    <div className="App">
-      <MovieCard title={title} genres={genresArr} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<MovieCard title={title} posterSrc={posterURL} genres={genresArr} />
+		</div>
+	);
 }
 
 export default App;
